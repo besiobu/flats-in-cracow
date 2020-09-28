38ce1cec393a9021d0e0e5352eebe5d530366805
besiobu <brian.esiobu@gmail.com>
2020-09-28 12:02:33 +0200

    Add setup script that creates data folder.

---
+++ setup.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+Path('flats-data').mkdir(parents=True, exist_ok=True)
+Path('flats-data/listings').mkdir(parents=True, exist_ok=True)
+Path('flats-data/urls').mkdir(parents=True, exist_ok=True)
